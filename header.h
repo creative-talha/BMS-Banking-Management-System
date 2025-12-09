ecc24152baf23d5878c49c3947e669d643c1980e
creative-talha <talhauetm@gmail.com>
2025-12-10 00:36:21 +0500

    Add all project files

---
+++ header.h
@@ -0,0 +1,6 @@
+#ifndef myfunc_h
+#define myfunc_h
+// header file
+void login();
+
+#endif
---
+++ login.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+// definition of password and id
+#define id 12345
+#define password "admin1"
+
+// login function
+void login() { // variables
+  int input_id, loop_counter = 1;
+  char input_password[10];
+
+  // loop
+  do {
+    // login inputs
+    printf("User id:");
+    scanf(" %d", &input_id);
+    printf("Password:");
+    scanf(" %9s", input_password);
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+      ;
+
+    // validity condition
+    if (input_id != id || strcmp(input_password, password))
+      printf("Invalid Credantial please try again!\n");
+    else
+      loop_counter--;
+
+  }
+  // loop running condition
+  while (loop_counter);
+}
